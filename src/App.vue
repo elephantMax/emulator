72d97928f99e211eb517230cc91c9ebe04e7db99
elephantMax <hatypovm12@mail.ru>
2020-12-02 17:51:45 +0300

    added Light component

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,17 @@
     <h1>Traffic Light Emulator</h1>
     <h3 class="timer">{{ timer }}</h3>
     <div class="lights">
-      <div
-        v-for="light in lights"
-        :key="light.url"
-        class="light"
-        :class="`${light.color} ${light.active ? 'active' : ''}`"
-      ></div>
+      <Light v-for="light in lights" :key="light.url" :light="light"/>
     </div>
   </div>
 </template>
 
 <script>
+import Light from "@/components/Light";
+
 export default {
   name: "App",
+  components: { Light },
   data: () => ({
     lights: [
       { color: "red", url: "/red", duration: 10, active: false },
@@ -32,15 +30,17 @@ export default {
       if (!this.$route.params.color) {
         this.$router.push("/red");
         this.activeLight = 0;
-        this.direction = 'bottom'
-        this.lights[this.activeLight].active = true
+        this.direction = "bottom";
+        this.lights[this.activeLight].active = true;
       }
 
       this.lights.forEach((item, index) => {
         if (this.$route.path === item.url) {
-          this.activeLight = index
-          this.lights[this.activeLight].active = true
-          this.activeLight === 2 ? this.direction = 'top' : this.direction = 'bottom'   
+          this.activeLight = index;
+          this.lights[this.activeLight].active = true;
+          this.activeLight === 2
+            ? (this.direction = "top")
+            : (this.direction = "bottom");
         }
       });
 
@@ -49,34 +49,30 @@ export default {
       this.timeout = setInterval(() => {
         this.timer = this.lights[this.activeLight].duration - counter;
         counter += 1;
-        console.log(this.activeLight);
-        if(this.timer <= 3) {
-          if(this.timer % 2 === 0) this.lights[this.activeLight].active = false
-          else this.lights[this.activeLight].active = true
+        if (this.timer <= 3) {
+          if (this.timer % 2 === 0)
+            this.lights[this.activeLight].active = false;
+          else this.lights[this.activeLight].active = true;
         }
 
         if (this.lights[this.activeLight].duration < counter) {
-          if(this.direction === 'bottom'){
-            if(this.activeLight >= 2){
-              this.direction = 'top'
-              this.activeLight -= 1
-            }
-            else this.activeLight += 1;
+          if (this.direction === "bottom") {
+            if (this.activeLight >= 2) {
+              this.direction = "top";
+              this.activeLight -= 1;
+            } else this.activeLight += 1;
+          } else {
+            if (this.activeLight === 0) {
+              this.direction = "bottom";
+              this.activeLight += 1;
+            } else this.activeLight--;
           }
-          else{
-            // console.log(1);
-            if(this.activeLight === 0){
-              this.direction = 'bottom'
-              this.activeLight += 1
-            }
-            else this.activeLight--;
-          }
-          
-          this.lights.forEach( light => light.active = false )
-          this.lights[this.activeLight].active = true
+
+          this.lights.forEach((light) => (light.active = false));
+          this.lights[this.activeLight].active = true;
           this.$router.push(this.lights[this.activeLight].url);
           counter = 1;
-          this.timer = this.lights[this.activeLight].duration
+          this.timer = this.lights[this.activeLight].duration;
         }
       }, 1000);
     }, 50);
@@ -112,28 +108,6 @@ body {
   align-items: center;
   background: rgb(30, 30, 31);
   padding: 20px;
-}
-.light {
-  margin-bottom: 10px;
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  opacity: 0.5;
-}
-.red {
-  background-color: red;
-  box-shadow: 1px 1px 20px red;
-}
-.yellow {
-  background-color: yellow;
-  box-shadow: 1px 1px 20px yellow;
-}
-.green {
-  background-color: rgb(3, 192, 3);
-  box-shadow: 1px 1px 20px rgb(1, 200, 1);
-}
-.active {
-  opacity: 1;
 }
 </style>
 
